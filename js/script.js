e2c9c2cccd152dde34c0ad9b7aa6bf9d25001bbc
Aurélien Luciani <aurelien.luciani@gmail.com>
2015-07-03 16:32:35 +1200

    babel + ace

---
+++ js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Full list of configuration options available at:
+  // https://github.com/hakimel/reveal.js#configuration
+  Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+
+    transition: 'slide', // none/fade/slide/convex/concave/zoom
+
+    // Optional reveal.js plugins
+    dependencies: [
+      { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+      { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+      { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+      { src: 'plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+      { src: 'plugin/zoom-js/zoom.js', async: true },
+      { src: 'plugin/notes/notes.js', async: true }
+    ]
+  });
+
+  function evalCode(text) {
+    babel.run(text);
+  }
+
+  function setup(slide) {
+    if (!slide) {
+      return;
+    };
+    editorEl = slide.querySelector('.editor');
+    if (!editorEl) {
+      return;
+    }
+
+    editor = ace.edit(editorEl);
+    window.editor = editor;
+  }
+
+  Reveal.addEventListener('slidechanged', function(e) {
+    setup(e.currentSlide);
+  });
+});
