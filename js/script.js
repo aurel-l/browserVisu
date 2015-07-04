bf033c4c709547afceb67020349cf1fc0e2236b1
Aurélien Luciani <aurelien.luciani@gmail.com>
2015-07-04 22:57:46 +1200

    cleanup and use of codepen iframe for interactive parts

--- js/script.js
+++ js/script.js
@@ -2,42 +2,54 @@ document.addEventListener('DOMContentLoaded', function() {
   // Full list of configuration options available at:
   // https://github.com/hakimel/reveal.js#configuration
   Reveal.initialize({
-    controls: true,
-    progress: true,
-    history: true,
-    center: true,
+    controls:    true,
+    progress:    true,
+    slideNumber: true,
+    history:     true,
+    center:      true,
+    keyboard:    true,
+    touch:       true,
+    help:        true,
 
     transition: 'slide', // none/fade/slide/convex/concave/zoom
 
     // Optional reveal.js plugins
     dependencies: [
-      { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
-      { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-      { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-      { src: 'plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
-      { src: 'plugin/zoom-js/zoom.js', async: true },
-      { src: 'plugin/notes/notes.js', async: true }
+      {src: 'plugin/zoom-js/zoom.js', async: true },
+      {src: 'plugin/notes/notes.js', async: true },
+      {src: 'plugin/remotes/remotes.js', async: true}
     ]
   });
 
-  function evalCode(text) {
-    babel.run(text);
-  }
-
-  function setup(slide) {
-    if (!slide) {
-      return;
-    };
-    editorEl = slide.querySelector('.editor');
-    if (!editorEl) {
-      return;
-    }
-
-    editor = ace.edit(editorEl);
-    window.editor = editor;
-  }
-
-  Reveal.addEventListener('slidechanged', function(e) {
-    setup(e.currentSlide);
-  });
+  // function evalCode(text) {
+  //   babel.run(text);
+  // }
+  // function getD3Ctx(slide) {
+  //   var el = slide.querySelector('.d3 > svg');
+  //   if (el) {
+  //     return d3.select(el);
+  //   }
+  // }
+  //
+  // function setup(slide) {
+  //   var d3ctx = window.d3ctx = getD3Ctx(slide);
+  //   console.log(d3ctx);
+  // }
+  //
+  // function cleanup(slide) {
+  //   var ta = slide.querySelector('textarea');
+  //   if (!ta) {
+  //     return;
+  //   }
+  //   //window.d3ctx = null;
+  // }
+  //
+  // Reveal.addEventListener('slidechanged', function(e) {
+  //   if (e.previousSlide) {
+  //     cleanup(e.previousSlide);
+  //   }
+  //   if (e.currentSlide) {
+  //     setup(e.currentSlide);
+  //   }
+  // });
 });
